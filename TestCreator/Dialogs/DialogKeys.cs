2e7dc435f244c5ace321045652254f4cacfc44f0
kostarsf <kostarsf@gmail.com>
2021-04-08 21:18:45 +0300

    TestCreator - Добавил реализацию диалоговых окон (сторонняя)

---
+++ TestCreator/Dialogs/DialogKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCreator.Dialogs
+{
+    public static class DialogKeys
+    {
+        public const string InputNumber = nameof(InputNumber);
+        public const string DisplayNumber = nameof(DisplayNumber);
+    }
+}
---
+++ TestCreator/Dialogs/Implementations/DialogData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCreator.Dialogs.Implementations
+{
+    public class DialogData
+    {
+        public DialogData(Func<object> viewModelFunc, Func<object> viewFunc)
+        {
+            ViewModelFunc = viewModelFunc;
+            ViewFunc = viewFunc;
+        }
+
+        public Func<object> ViewModelFunc { get; }
+
+        public Func<object> ViewFunc { get; }
+    }
+}
